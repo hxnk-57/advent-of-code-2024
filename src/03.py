02174ff82b907fa4f2d2a61eaa91756dc235c598
henk14 <henkb5@discovery.co.za>
2024-12-03 09:03:42 +0200

    different appproach

--- src/03.py
+++ src/03.py
@@ -2,33 +2,27 @@ import re
 
 file_path = "input/03.txt"
 
-content = open(file_path).read().strip()
+with open(file_path, 'r') as file:
+    content = file.read().strip()
+
+def calculate_score(text: str) -> int:
+    total = 0
+    for i in range(len(text)):
+        matches = re.match(r'mul\((\d{1,3}),(\d{1,3})\)', text[i:])
+        if matches:
+            arg_1, arg_2 = int(matches.group(1)), int(matches.group(2))
+            total += arg_1 * arg_2
+    return total    
+
 
 def part_one() -> int:
-    total = 0
-    for i in range(len(content)):
-        matches = re.match(r'mul\((\d{1,3}),(\d{1,3})\)', content[i:])
-        if matches:
-            arg_1 = int(matches.group(1))
-            arg_2 = int(matches.group(2))
-            total += arg_1 * arg_2
-    return total
+    return calculate_score(content)
 
 
 def part_two() -> int:
-    active = True
-    total = 0
-    for i in range(len(content)):
-        if content[i:].startswith("do()"):
-            active = True
-        if content[i:].startswith("don't()"):
-            active = False
-        matches = re.match(r'mul\((\d{1,3}),(\d{1,3})\)', content[i:])
-        if matches and active:
-            arg_1 = int(matches.group(1))
-            arg_2 = int(matches.group(2))
-            total += arg_1 * arg_2 
-    return total
+    segments = ("do()" + content).split("don't()")
+    clean_text = "".join(segment[segment.find("do()"):] for segment in segments)
+    return calculate_score(clean_text)
 
 if __name__ == "__main__":
     print(f"Part One: {part_one()}")
